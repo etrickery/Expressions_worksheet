f25b94d948beb5a47d88d793de677f4e9cea6586
Etrickery <ericspfister@gmail.com>
2015-02-09 19:06:49 -0500

    Polished html
    Polished html to remove some console errors and setup rectangle area
    formula to calculate area of a rectangle.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Eric Pfister
+//February 9th, 2015
+//Activity: Expression Worksheet
+
+
+//Calculate the area of a rectangle
+var width = 20;
+var height = 15;
+
+//Multiply the width x height to determine are
+var area = width * height;
+
+//Display the results in the console
+console.log(area);
+
+
