4b2790ac02d800c94afc0f8a174fa9e553480d72
Etrickery <ericspfister@gmail.com>
2015-02-09 20:56:41 -0500

    Polishing js file
    Working on cleaning up js file

--- js/script.js
+++ js/script.js
@@ -1,31 +1,48 @@
 //Eric Pfister
 //February 9th, 2015
+//SDI 1502
+//Section 01
 //Activity: Expression Worksheet
 
 
 //Calculate the area of a rectangle
 //Define variables (prompt for input)
-var width = prompt("Enter width of rectangle");
-var height = prompt("Enter height of rectangle");
+var width;
+var height;
+var area;
+var resultExample;
 
-//Multiply the width x height to determine are
-var area = width * height;
+//Request user prompts for rectangle size
+width = prompt("Enter the width of the rectangle: ", "20");
+height = prompt("Enter the height of the rectangle: ", "15");
+
+//Multiply the width x height to determine area and build the result statement
+area = width * height;
+resultExample = "The area of a rectangle who's width equals " + (width) + " and height equals " + (height) + ", will have an area of " + (area) + ".";
 
 //Display the results in the console
-console.log(area);
+console.log(resultExample);
 
 
-//"Dog years" problem
+
+//Dog years problem
 //Define variables (prompt for input)
 //dogConstant is a constant that is used to calculate based on 1 human year = 7 dog years
 var dogConstant = 7;
-var sparkyAge = prompt("Enter Sparky's age in actual years");
+var sparkyAge;
+
+//Prompt the user for a value to define sparkyAge
+sparkyAge = prompt("Enter Sparky's age in actual years", "7");
 
 //Calculate Sparky's age in dog years
+//The first variable is the solution to the dog years problem
+//The second variable is the ouput to be displayed via console
+//(This formula is inserting the variables, "sparkyAge" and "dogYears" into the statement through concatenating)
 var dogYears = dogConstant * sparkyAge;
+var result2 = "Sparky is " + (sparkyAge) + " human years old, which is " + (dogYears) + " in dog years.";
 
 //Display answer in console log
-console.log(dogYears);
+console.log(result2);
 
 
 
